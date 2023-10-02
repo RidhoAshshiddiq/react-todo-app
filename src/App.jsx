eeb1d46e2bb5018124a6c99381448419f6d4b69e
RidhoAshshiddiq <140096398+RidhoAshshiddiq@users.noreply.github.com>
2023-10-02 20:18:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import React, { useState } from 'react';
+
 
 function TodoApp() {
   // State untuk menyimpan daftar tugas
