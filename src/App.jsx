af6441ec6a2863b22f4349488182224852c8a95c
Ridho Ashshiddiq <lusiaanda46@gmail.com>
2023-10-10 22:48:17 +0700

    Update semuanya

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,127 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class TodoApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      newTodo: ''
-    };
-  }
+// class TodoApp extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       todos: [],
+//       newTodo: ''
+//     };
+//   }
 
-  handleChange = (event) => {
-    this.setState({ newTodo: event.target.value });
-  }
+//   handleChange = (event) => {
+//     this.setState({ newTodo: event.target.value });
+//   }
 
-  addTodo = () => {
-    if (this.state.newTodo !== '') {
-      this.setState((prevState) => ({
-        todos: [...prevState.todos, this.state.newTodo],
-        newTodo: ''
-      }));
+//   addTodo = () => {
+//     if (this.state.newTodo !== '') {
+//       this.setState((prevState) => ({
+//         todos: [...prevState.todos, this.state.newTodo],
+//         newTodo: ''
+//       }));
+//     }
+//   }
+
+//   deleteTodo = (index) => {
+//     const newTodos = [...this.state.todos];
+//     newTodos.splice(index, 1);
+//     this.setState({ todos: newTodos });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>To-Do List</h1>
+//         <input
+//           type="text"
+//           placeholder="Add a new task"
+//           value={this.state.newTodo}
+//           onChange={this.handleChange}
+//         />
+//         <button onClick={this.addTodo}>Add To Do</button>
+//         <ul>
+//           {this.state.todos.map((todo, index) => (
+//             <li key={index}>
+//               {todo}
+//               <button onClick={() => this.deleteTodo(index)}>Delete</button>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TodoApp;
+
+import React, { useState } from 'react';
+import './App.css';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  const addTodo = () => {
+    if (todo.trim() !== '') {
+      setTodos([...todos, { text: todo, completed: false }]);
+      setTodo('');
     }
-  }
+  };
 
-  deleteTodo = (index) => {
-    const newTodos = [...this.state.todos];
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
     newTodos.splice(index, 1);
-    this.setState({ todos: newTodos });
-  }
+    setTodos(newTodos);
+  };
 
-  render() {
-    return (
+  const toggleCompletion = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const clearCompleted = () => {
+    const newTodos = todos.filter((task) => !task.completed);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>My ToDo List</h1>
       <div>
-        <h1>To-Do List</h1>
         <input
           type="text"
-          placeholder="Add a new task"
-          value={this.state.newTodo}
-          onChange={this.handleChange}
+          placeholder="Tambahkan Tugas Anda"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
         />
-        <button onClick={this.addTodo}>Add To Do</button>
-        <ul>
-          {this.state.todos.map((todo, index) => (
-            <li key={index}>
-              {todo}
-              <button onClick={() => this.deleteTodo(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
+        <button onClick={addTodo}>Tambahkan Tugas</button>
       </div>
-    );
-  }
+      <ul>
+        {todos.map((task, index) => (
+          <li key={index} className={task.completed ? 'selesai' : ''}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleCompletion(index)}
+            />
+            <span
+              style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
+            >
+              {task.text}
+            </span>
+            <button onClick={() => removeTodo(index)}>Hapus</button>
+          </li>
+        ))}
+      </ul>
+      {todos.some(task => task.completed) && (
+        <button onClick={clearCompleted}>Hapus yang Sudah Selesai</button>
+      )}
+    </div>
+  );
 }
 
 export default TodoApp;
-
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+
+  base: '/react-todo-app/',
+
+  server: {
+    port: 3000,
+  },
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+};
