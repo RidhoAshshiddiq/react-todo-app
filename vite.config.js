29008e6fd2e7796ce855c624bf70780a89c259e9
RidhoAshshiddiq <140096398+RidhoAshshiddiq@users.noreply.github.com>
2023-10-16 20:59:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import React from '@vitejs/plugin-react';
+import React from '@vitejs/plugin-react'
 
-export default {
-  plugins: [React({ detectReact: true })],
-
-  base: '/react-todo-app/',
-
-  server: {
-    port: 3000,
-  },
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
