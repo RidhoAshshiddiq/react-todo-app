d48e7811fc1b8059c84281181db51b6b1a9b3471
RidhoAshshiddiq <140096398+RidhoAshshiddiq@users.noreply.github.com>
2023-10-16 21:31:20 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/react-todo-app/' : '/',
-})
+});
+
