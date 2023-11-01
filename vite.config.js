11602de04342cf36dbfb2367f4fe9abe49de9483
Ridho Ashshiddiq <lusiaanda46@gmail.com>
2023-11-01 21:30:01 +0700

    Add configuration for github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import React from '@vitejs/plugin-react';
 
-export default {
-  plugins: [React({ detectReact: true })],
-
-  base: '/react-todo-app/',
-
-  server: {
-    port: 3000,
-  },
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/react-todo-app/', // Nama repository ditambahkan
+})
