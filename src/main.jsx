bc31a4f545c28f401835f9029bba48f772d2abc8
Ridho Ashshiddiq <lusiaanda46@gmail.com>
2023-11-01 21:53:52 +0700

    Resolve conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import TodoApp from './App.jsx'
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <TodoApp />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import TodoApp from './App.jsx'
 
-const rootElement = document.getElementById('root');
 
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <TodoApp />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element with ID 'root' not found.");
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import TodoApp from './App.jsx'
+
+// const rootElement = document.getElementById('root');
+
+// if (rootElement) {
+//   ReactDOM.createRoot(rootElement).render(
+//     <React.StrictMode>
+//       <TodoApp />
+//     </React.StrictMode>
+//   );
+// } else {
+//   console.error("Root element with ID 'root' not found.");
+// }
